b5ad78b4115f62bad9b5386ce0661997a42a4350
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-06-09 22:57:09 -0300

    feat: signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,15 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDTO): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    await user.save();
+  }
+}
