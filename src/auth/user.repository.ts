df59d4eedd9196f208996e0d8fde13aa2f2de42f
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-06-12 23:45:08 -0300

    feat: deploying on amazon

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,7 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import {
   ConflictException,
   InternalServerErrorException,
